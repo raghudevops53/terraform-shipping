517fb3ba998cd3544df98e5648a347243e3e0fe7
KalluruS <>
2020-12-31 08:22:13 +0530

    Update terraform-project/terraform-shipping/

--- asg.tf
+++ asg.tf
@@ -9,7 +9,7 @@ module "asg" {
   HEALTH          = "/health"
 }
 
-resource "aws_lb_listener_rule" "catalogue" {
+resource "aws_lb_listener_rule" "shipping" {
   listener_arn          = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_LISTENER_ARN
   action                  {
     type                = "forward"
